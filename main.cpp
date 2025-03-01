87517be45c1bb48b7cefff030cd76585c04de691
Kiril Mishevski <mishevski0800@gmail.com>
2025-03-01 07:12:18 +0100

    Add delete and list action

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <ctime>
+#include <optional>
 #include <vector>
+#include <format>
+#include <iomanip>
+#include <unordered_map>
+#include <sstream>
 
 // NOTE: Basic Implementation, code should be refactored and restructured in the future
-// Features to change/add: change tasks to a better data structure, add exception handling instead of return code
+// Features to change/add: change tasks to a better data structure, add exception handling instead of return code, refactor everything in more header/source files
 
 enum TaskStatus {
     TODO, IN_PROGRESS, DONE
 };
+
+std::string statusToString(TaskStatus status) {
+    static  const std::unordered_map<TaskStatus, std::string> statusMap = {
+        {TaskStatus::DONE, "Done"},
+        {TaskStatus::TODO, "Todo"},
+        {TaskStatus::IN_PROGRESS, "In progress"},
+    };
+
+    if (auto it = statusMap.find(status); it != statusMap.end()) {
+        return it->second;
+    }
+
+    return "Unknown";
+}
 
 class Task {
 private:
@@ -45,8 +64,20 @@ public:
         return this->status;
     }
 
+    std::string getStatusString() {
+        return statusToString(this->status);
+    }
+
     void setStatus(TaskStatus status) {
         this->status = status;
+    }
+
+    std::string formatTime(std::time_t time) {
+        std::tm* localTime = std::localtime(&time);
+        std::ostringstream oss;
+        oss << std::put_time(localTime, "%Y/%m/%d %H:%M:%S");
+
+        return oss.str();
     }
 
     std::time_t getCreatedAt() {
@@ -57,6 +88,10 @@ public:
         this->createdAt = createdAt;
     }
 
+    std::string getCreatedAtFormated() {
+        return formatTime(this->getCreatedAt());
+    }
+
     std::time_t getUpdatedAt() {
         return this->updatedAt;
     }
@@ -64,10 +99,28 @@ public:
     void setUpdatedAt(std::time_t updatedAt) {
         this->updatedAt = updatedAt;
     }
+
+    std::string getUpdatedAtFormated() {
+        return formatTime(this->getUpdatedAt());
+    }
 };
 
 // Change to binary tree later on for faster search
 std::vector<Task> tasks;
+
+struct FindTaskByIdResponse {
+    Task& task;
+    int index;
+};
+
+std::optional<FindTaskByIdResponse> findTaskById(int id) {
+    for (int i = 0; i < tasks.size(); i++) {
+        if (tasks[i].getId() == id) {
+            return FindTaskByIdResponse{tasks[i], i};
+        }
+    }
+    return std::nullopt;
+}
 
 int add(int argc, char *argv[]) {
     if (argc < 3) {
@@ -82,7 +135,7 @@ int add(int argc, char *argv[]) {
     return 0;
 }
 
-int upddate(int argc, char *argv[]) {
+int update(int argc, char *argv[]) {
     if (argc < 4) {
         std::cerr << "Usage: ./task-cli update <id> <description>" << std::endl;
         return 1;
@@ -91,13 +144,42 @@ int upddate(int argc, char *argv[]) {
     int id = std::stoi(argv[2]);
     std::string description = static_cast<std::string>(argv[3]);
 
-    for (Task& task : tasks) {
-        if (task.getId() == id) {
-            task.setDescription(description);
-        }
+    std::optional<FindTaskByIdResponse> response = findTaskById(id);
+
+    if (!response) {
+        std::cerr << "Task not found" << std::endl;
     }
 
+    response->task.setDescription(description);
+
     return 0;
+}
+
+int deleteTask(int argc, char *argv[]) {
+    if (argc < 3) {
+        std::cerr << "Usage: ./task-cli delete <id>" << std::endl;
+        return 1;
+    }
+
+    int id = std::stoi(argv[2]);
+
+    std::optional<FindTaskByIdResponse> response = findTaskById(id);
+
+    if (!response) {
+        std::cerr << "Task not found" << std::endl;
+        return 1;
+    }
+
+    tasks.erase(tasks.begin() + response->index);
+
+    return 0;
+}
+
+void list() {
+    for (Task task : tasks) {
+        std::string message = std::format("Task: ( id: {}, status: {}, description: {}, createdAt: {}, updatedAt: {} )", task.getId(), task.getStatusString(), task.getDescription(), task.getCreatedAtFormated(), task.getUpdatedAtFormated());
+        std::cout << message << std::endl;
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -118,16 +200,15 @@ int main(int argc, char *argv[]) {
     if (action == "add") {
         add(argc, argv);
     }else if (action == "update") {
-        upddate(argc, argv);
+        update(argc, argv);
+    }else if (action == "delete") {
+        deleteTask(argc, argv);
+    }else if (action == "list") {
+        list();
     }
 
-    for (int i = 0; i < tasks.size(); ++i) {
-        std::cout << tasks.at(i).getId() << std::endl;
-        std::cout << tasks.at(i).getDescription() << std::endl;
-        std::cout << tasks.at(i).getStatus() << std::endl;
-        std::cout << tasks.at(i).getCreatedAt() << std::endl;
-        std::cout << tasks.at(i).getUpdatedAt() << std::endl;
-    }
+    // For debugging only
+    list();
 
     std::cout << "Main is working" << std::endl;
 
